29144e0f9190136328ab63542b1b3c6bb64d68e2
vht3036 <156178809+vht3036@users.noreply.github.com>
2025-09-20 11:19:32 +0100

    buff Wartortle

--- data/pokemon/base_stats/wartortle.asm
+++ data/pokemon/base_stats/wartortle.asm
@@ -5,9 +5,9 @@
 	db 45 ; catch rate
 	db 143 ; base exp
 	db NO_ITEM, NO_ITEM ; held items
-	dn GENDER_F12_5, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F50, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
 
-	abilities_for WARTORTLE, TORRENT, RAIN_DISH, SHELL_ARMOR
+	abilities_for WARTORTLE, TORRENT, SHELL_ARMOR, RAIN_DISH
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_MONSTER, EGG_WATER_1 ; egg groups
 
