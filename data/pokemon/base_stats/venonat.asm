b86c1b4890e55f65015c45819d02596858201d37
vht3036 <156178809+vht3036@users.noreply.github.com>
2025-10-05 15:53:46 +0100

    change Venonat's typing to Bug/Psychic and give it Poison Heal as its second ability

--- data/pokemon/base_stats/venonat.asm
+++ data/pokemon/base_stats/venonat.asm
@@ -1,13 +1,17 @@
 	db  60,  55,  50,  45,  40,  55 ; 305 BST
 	;   hp  atk  def  spe  sat  sdf
 
+if DEF(FAITHFUL)
 	db BUG, POISON ; type
+else
+	db BUG, PSYCHIC ; type
+endc
 	db 190 ; catch rate
 	db 75 ; base exp
 	db NO_ITEM, NO_ITEM ; held items
 	dn GENDER_F50, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
 
-	abilities_for VENONAT, COMPOUND_EYES, TINTED_LENS, RUN_AWAY
+	abilities_for VENONAT, COMPOUND_EYES, POISON_HEAL, TINTED_LENS
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_BUG, EGG_BUG ; egg groups
 
